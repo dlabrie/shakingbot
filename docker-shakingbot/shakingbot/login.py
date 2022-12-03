35aeeef84004eadaad3709ea663af184f69b38fa
Daniel <hydra@reft.io>
2022-12-03 01:45:01 -0500

    Update login.py
    General clean up

--- docker-shakingbot/shakingbot/login.py
+++ docker-shakingbot/shakingbot/login.py
@@ -1,20 +1,18 @@
+import json
+import jwt
+import os
+
 from getpass import getpass
 from modules.shakepay import *
 from modules.telegramnotif import *
-import json
-import jwt
 
-import os
+if not os.path.isfile("creds/.uuid"):
+    with open("creds/.uuid", "w") as f:
+        f.write("")
 
-if os.path.isfile("creds/.uuid") != True:
-    f = open("creds/.uuid", "w")
-    f.write("")
-    f.close()
-
-if os.path.isfile("creds/.jwtToken") != True:
-    f = open("creds/.jwtToken", "w")
-    f.write("")
-    f.close()
+if not os.path.isfile("creds/.jwtToken"):
+    with open("creds/.jwtToken", "w") as f:
+        f.write("")
 else:
     if getJWT() != "":
         print("Looks like you already have a session, delete creds/.jwtToken to force this")
@@ -23,35 +21,32 @@ else:
 shakepayUsername = input("Shakepay Username: ")
 shakepayPassword = getpass("Shakepay Password: ")
 
-reply = telegramApiToken("Would you like to receive notifications via Telegram when a successful shake occurs?")
+if telegramApiToken("Would you like to receive notifications via Telegram when a successful shake occurs?"):
+    ## Login with provided credentials
+    print("Sending initial login request")
 
-## Login with provided credentials
-print("Sending initial login request")
+    response = shakepayAPIAuth(shakepayUsername, shakepayPassword)
+    accessResponse = json.loads(response.text)
 
-response = shakepayAPIAuth(shakepayUsername, shakepayPassword)
-accessResponse = json.loads(response.text)
-formatted_accessResponse = json.dumps(accessResponse)
+    if "accessToken" in accessResponse:
+        saveJWT(accessResponse["accessToken"])
 
-if "accessToken" in accessResponse:
-    saveJWT(accessResponse["accessToken"])
+        jwtAccessToken = jwt.decode(accessResponse["accessToken"], algorithms="HS256", options={"verify_signature": False})
 
-    jwtAccessToken = jwt.decode(accessResponse["accessToken"], algorithms="HS256", options={"verify_signature": False})
+        if jwtAccessToken["mfa"] == True:
+            smsCode = input("Login is being MFA'd, please enter your SMS code [numbers only, no dash] : ")
+            response = shakepayAPIPost("/authentication", {"strategy":"mfa","mfaToken":smsCode})
 
-    if jwtAccessToken["mfa"]==True:
-        smsCode = input("Login is being MFA'd, please enter your SMS code [numbers only, no dash] : ")
-        response = shakepayAPIPost("/authentication", {"strategy":"mfa","mfaToken":smsCode})
+            accessResponse = json.loads(response.text)
 
-        accessResponse = json.loads(response.text)
+            if "accessToken" in accessResponse:
+                saveJWT(accessResponse["accessToken"])
 
-        if "accessToken" in accessResponse:
-            saveJWT(accessResponse["accessToken"])
+                jwtAccessToken = jwt.decode(accessResponse["accessToken"], algorithms="HS256", options={"verify_signature": False})
 
-            jwtAccessToken = jwt.decode(accessResponse["accessToken"], algorithms="HS256", options={"verify_signature": False})
-            
-        else:
-            print(accessResponse)
+            else:
+                print(accessResponse)
 
-            
-else:
-    shakeAPIMessage = json.loads(formatted_accessResponse)
-    print(shakeAPIMessage['message'])
+    else:
+        shakeAPIMessage = json.loads(response.text)
+        print(shakeAPIMessage['message'])
